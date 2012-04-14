d1f0bea8444d371e882a4419eb8e02a7866a19f6
Daniel Turner <dst502@york.ac.uk>
2012-04-14 17:53:50 +0100

    Modified prop_contains with the help of ClaudiusMaximus from #haskell

--- binarySearchTests.hs
+++ binarySearchTests.hs
@@ -2,8 +2,9 @@ import BinarySearch (binarySearch)
 import Data.List
 import Test.QuickCheck
 
-prop_contains :: [Int] -> Int -> Property
-prop_contains xs x = x `elem` xs ==> binarySearch (sort xs) x
+prop_contains :: [Int] -> Property
+--prop_contains xs x = x `elem` xs ==> binarySearch (sort xs) x
+prop_contains xs = True ==> let ys = sort xs in and [ binarySearch ys y | y <- ys ]
 
 prop_not_contains :: [Int] -> Int -> Property
 prop_not_contains xs x = x `notElem` xs ==> not $ binarySearch (sort xs) x
